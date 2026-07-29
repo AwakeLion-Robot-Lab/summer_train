e0c0dcf761934e3cf8177ef6bcb51968776b4fa7
Bruce0178 <243039524+Bruce0178@users.noreply.github.com>
2026-07-29 21:18:36 +0800

    update l4_planning pro max

--- src/l5_control/controller.cpp
+++ src/l5_control/controller.cpp
@@ -4,9 +4,7 @@ namespace L5Control {
 
 SerialCommand Controller::makeCommand(const L4Planning::AimPlan& plan) const
 {
-  const bool shoot =
-    plan.valid && plan.fire_permitted && !plan.armor_switching;
-  return SerialCommand{plan.yaw, plan.pitch, shoot};
+  return SerialCommand{plan.yaw, plan.pitch, plan.valid};
 }
 
 }  // namespace L5Control
